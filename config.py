52540b2b20acbf42a918122e43adc9554a13c658
nasim <nasimkrut@gmail.com>
2025-10-07 16:17:12 +0500

    заного

--- config.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = "8070552404:AAHY4McU5mu5AubeZHxIyBI4SgBzzPK0NYQ"
-DATABASE_URL = "postgresql+asyncpg://postgres:Moplicop123!@localhost:5432/FoodFitness"
-GEMINI_API_KEY = "AIzaSyD7dlErksI3kxWCu5rga9WZqcndSxjZXu4"
